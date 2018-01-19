a86eb61bb059a428fbd1c95d2beb09d76b4ab5c5
Romano Kleinwächter <romano.kleinwaechter@gmail.com>
2018-01-19 13:36:24 +0100

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+Implementation for the Google Tag Manager.
---
+++ google-tag-manager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Grav\Plugin;
+
+
+use Grav\Common\Page\Types;
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+class GoogleTagManagerPlugin extends Plugin
+{
+    /**
+     * @inheritdoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
+        ];
+    }
+
+    /**
+     *
+     */
+    public function onPluginsInitialized()
+    {
+        if ($this->isAdmin()) {
+            $this->active = false;
+            return;
+        }
+
+        $this->enable([
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+        ]);
+    }
+
+    /**
+     * Add current directory to twig lookup paths.
+     */
+    public function onTwigTemplatePaths()
+    {
+        $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+}
